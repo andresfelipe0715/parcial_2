355b9e549d7075ff3d68e206ebacf864aea80b5b
Joseph Suescun <jjsuescunb@ufpso.edu.co>
2022-11-23 22:17:53 -0500

    Creacion de controlador y servicios de usuario

---
+++ primer_parcial_almacen-main/src/main/java/com/example/primer_parcial7/repository/UsuarioRepository.java
@@ -0,0 +1,14 @@
+package com.example.primer_parcial7.repository;
+import com.example.primer_parcial7.models.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UsuarioRepository extends JpaRepository <Usuario,Long> {
+    List<Usuario> findAllByNombre(String nombre);
+
+    List<Usuario> findAllByApellidos(String apellidos);
+
+    Usuario findByCorreo(String correo);
+
+}
